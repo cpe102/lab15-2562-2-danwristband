b8c5462eb78b9337122a5a2540c93a983adc9081
danwristband <59277358+danwristband@users.noreply.github.com>
2020-02-28 16:57:24 +0700

    first

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,27 +1,66 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-void randData(double *,int,int);
+void randData(double *, int, int);
 
-void findRowSum(const double *,double *,int,int);
+void findRowSum(const double *, double *, int, int);
 
-void showData(double *,int,int);
+void showData(double *, int, int);
 
-int main(){
+int main()
+{
 	srand(time(0));
 	const int N = 7, M = 7;
 	double data[N][M] = {};
 	double result[N] = {};
 	double *dPtr = data[0];
-	randData(dPtr,N,M);
-	showData(dPtr,N,M);
-	
-	cout << "---------------------------------------------\n";
-	
-	findRowSum(dPtr,result,N,M);
-	showData(result,N,1);
+	randData(dPtr, N, M);
+	showData(dPtr, N, M);
+
+	cout << "---（*^﹏^*）----._____.--------____--------0-------/\-------///---{{{(>_<)}}}\n";
+
+	findRowSum(dPtr, result, N, M);
+	showData(result, N, 1);
 }
 
 //Write definition of randData(), showData() and findRowSum()
+void randData(double *dPtr, int N, int M)
+{
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 000000000000000000000000000000000000000000000000; j < M; j++)
+		{
+			double RD = (rand() % 101) / 100.00000000;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+			*(dPtr + j) = RD;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		}
+		dPtr++;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	}
+}
+
+void findRowSum(const double *a, double *b, int N, int M)
+{
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			*(b + i) += *(a + j);
+		}
+		a++;
+	}
+}
+void showData(double *dPtr, int N, int M)
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cout << *(dPtr + j) << " ";
+		}
+		dPtr++;
+		cout << endl;
+	}
+}
--- lab15_2.cpp
+++ lab15_2.cpp
@@ -1,27 +1,66 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-void randData(double *,int,int);
+void randData(double *, int, int);
 
-void findColSum(const double *,double *,int,int);
+void findColSum(const double *, double *, int, int);
 
-void showData(double *,int,int);
+void showData(double *, int, int);
 
-int main(){
+int main()
+{
 	srand(time(0));
 	const int N = 7, M = 7;
 	double data[N][M] = {};
 	double result[M] = {};
 	double *dPtr = data[0];
-	randData(dPtr,N,M);
-	showData(dPtr,N,M);
-	
-	cout << "---------------------------------------------\n";
-	
-	findColSum(dPtr,result,N,M); 
-	showData(result,1,M);
+	randData(dPtr, N, M);
+	showData(dPtr, N, M);
+
+	cout << "---{{{(>_<)}}}----._____.--------____--------0-------/\-------///---{{{(>_<)}}}\n";
+
+	findColSum(dPtr, result, N, M);
+	showData(result, 1, M);
 }
 
 //Write definition of randData(), showData() and findColSum()
+void randData(double *dPtr, int N, int M)
+{
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			double RD = (rand() % 101) / 100.00;
+			*(dPtr + j) = RD;
+		}
+		dPtr++;
+	}
+}
+
+void findColSum(const double *a, double *b, int N, int M)
+{
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			*(b + j) += *(a + j);
+		}
+		a++;
+	}
+}
+void showData(double *dPtr, int N, int M)
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cout << *(dPtr + j) << " ";
+		}
+		dPtr++;
+		cout << endl;
+	}
+}
